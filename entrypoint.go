38f8848922f12e1ac81c6310365e5da24cc5e0ca
Knicholas Kennedy <knicholas.kennedy@gmail.com>
2025-05-20 20:29:39 -0400

    Added entitlement key to env
    Signed-off-by: Knicholas Kennedy <knicholas.kennedy@gmail.com>

--- entrypoint.go
+++ entrypoint.go
@@ -8,13 +8,28 @@ import (
 )
 
 func main() {
-	value, exists := os.LookupEnv("CONFIG_DIR")
+	configDir, exists := os.LookupEnv("CONFIG_DIR")
 	if !exists {
 		log.Fatal("CONFIG_DIR not set correctly.")
 	}
-	command := exec.Command("ansible-playbook", "playbooks/deploy_sterling_b2bi.yml", "--extra-vars", "config_dir="+value)
+
+	argsSlice := []string{"playbooks/deploy_sterling_b2bi.yml"}
+	argsSlice = append(argsSlice, "-e")
+	argsSlice = append(argsSlice, "config_dir="+configDir)
+
+	entitlementKey, exists := os.LookupEnv("ENTITLEMENT_KEY")
+	if !exists {
+		log.Fatal("ENTITLEMENT_KEY not set correctly.")
+	}
+
+	argsSlice = append(argsSlice, "-e")
+	argsSlice = append(argsSlice, "entitlement_key="+entitlementKey)
+
+	command := exec.Command("ansible-playbook", argsSlice...)
 
 	fmt.Println(command)
+
+	fmt.Println(os.Args)
 
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
